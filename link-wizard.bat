4e69498a28fb0c07ea7eb555799e2e260257cea4
Jared Tyler <jtyler@wdca.ca>
2012-02-12 16:38:27 -0500

    Adding a batch script to symlink mod files into a Minecraft code directory

---
+++ link-wizard.bat
@@ -0,0 +1,33 @@
+@echo off
+
+pushd .
+
+set source_path="C:\Users\jtyler\Documents\Programming\Java\Minecraft\Wizard"
+set dest_path=".\src\minecraft\net\minecraft\src"
+
+
+REM remove old links or files
+del /s /q  %dest_path%\BlockHolyLight.java
+del /s /q  %dest_path%\ItemExplosiveStaff.java
+del /s /q  %dest_path%\ItemFireStaff.java
+del /s /q  %dest_path%\ItemHolyStaff.java
+del /s /q  %dest_path%\ItemLightningStaff.java
+del /s /q  %dest_path%\ItemTeleportationStaff.java
+del /s /q  %dest_path%\ItemWizardStaff.java
+del /s /q  %dest_path%\mod_Wizard.java
+
+
+REM rebuild links
+mklink  %dest_path%\BlockHolyLight.java  %source_path%\BlockHolyLight.java
+mklink  %dest_path%\ItemExplosiveStaff.java  %source_path%\ItemExplosiveStaff.java
+mklink  %dest_path%\ItemFireStaff.java  %source_path%\ItemFireStaff.java
+mklink  %dest_path%\ItemHolyStaff.java  %source_path%\ItemHolyStaff.java
+mklink  %dest_path%\ItemLightningStaff.java  %source_path%\ItemLightningStaff.java
+mklink  %dest_path%\ItemTeleportationStaff.java  %source_path%\ItemTeleportationStaff.java
+mklink  %dest_path%\ItemWizardStaff.java  %source_path%\ItemWizardStaff.java
+mklink  %dest_path%\mod_Wizard.java  %source_path%\mod_Wizard.java
+
+
+popd
+
+pause
