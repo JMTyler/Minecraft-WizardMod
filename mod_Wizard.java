16642966211cc0f77b52fe9b60046602260fc1ee
Jared Tyler <jtyler@wdca.ca>
2012-02-12 16:37:56 -0500

    Using the new functionality of addArmor and addWeapon to add item title tooltips

--- mod_Wizard.java
+++ mod_Wizard.java
@@ -9,8 +9,6 @@ public class mod_Wizard extends BaseMod
 {
 	public static final String armorGroup = "wizard";
 	
-	public static final Block holyLight;
-	
 	public static Item lightningStaff;
 	public static Item explosiveStaff;
 	public static Item fireStaff;
@@ -25,52 +23,32 @@ public class mod_Wizard extends BaseMod
 	
 	static
 	{
-		// don't need this anymore but haven't tested removing it yet
-		holyLight = (new BlockHolyLight()).setBlockName("holyLight")
-				.setHardness(0.0F)
-				.setResistance(0.0F)
-				.setLightValue(1.0F)
-				.setRequiresSelfNotify();
-		
 		if (mod_Classes.addArmorGroup(armorGroup)) {
-			wizardHat = mod_Classes.addArmor("wizardHat", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, ArmorType.Helmet, 0));
-			wizardRobe = mod_Classes.addArmor("wizardRobe", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, ArmorType.Plate, 0));
-			wizardPantaloons = mod_Classes.addArmor("wizardPantaloons", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, ArmorType.Legs, 0));
-			wizardBooties = mod_Classes.addArmor("wizardBooties", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, ArmorType.Boots, 0));
+			wizardHat = mod_Classes.addArmor("wizardHat", "Wizard's Hat", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Helmet, 0));
+			wizardRobe = mod_Classes.addArmor("wizardRobe", "Robe of Mists", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Plate, 0));
+			wizardPantaloons = mod_Classes.addArmor("wizardPantaloons", "Magical Pantaloons", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Legs, 0));
+			wizardBooties = mod_Classes.addArmor("wizardBooties", "Lil' Booties", new ItemClassArmor(mod_Classes.getUniqueItemId(), armorGroup, EnumArmorType.Boots, 0));
 			
 			if (wizardHat != null) {
-				mod_Classes.addWeapon("lightningStaff", new ItemLightningStaff()).dependsOn((ItemClassArmor)wizardHat);
-				mod_Classes.addWeapon("explosiveStaff", new ItemExplosiveStaff()).dependsOn((ItemClassArmor)wizardHat);
-				mod_Classes.addWeapon("fireStaff", new ItemFireStaff()).dependsOn((ItemClassArmor)wizardHat);
-				mod_Classes.addWeapon("holyStaff", new ItemHolyStaff()).dependsOn((ItemClassArmor)wizardHat);
-				mod_Classes.addWeapon("teleportStaff", new ItemTeleportationStaff()).dependsOn((ItemClassArmor)wizardHat);
+				lightningStaff = mod_Classes.addWeapon("lightningStaff", "Lightning Staff", new ItemLightningStaff()).dependsOn((ItemClassArmor)wizardHat);
+				explosiveStaff = mod_Classes.addWeapon("explosiveStaff", "Explosive Staff", new ItemExplosiveStaff()).dependsOn((ItemClassArmor)wizardHat);
+				fireStaff = mod_Classes.addWeapon("fireStaff", "Fire Staff", new ItemFireStaff()).dependsOn((ItemClassArmor)wizardHat);
+				holyStaff = mod_Classes.addWeapon("holyStaff", "Staff of Holy Light", new ItemHolyStaff()).dependsOn((ItemClassArmor)wizardHat);
+				teleportStaff = mod_Classes.addWeapon("teleportStaff", "Staff of Teleportation", new ItemTeleportationStaff()).dependsOn((ItemClassArmor)wizardHat);
 			}
 		}
 	}
 	
 	public mod_Wizard()
 	{
-		/* Setting up blocks */
-		ModLoader.RegisterBlock(holyLight);
-		
-		
-		/* Setting up items */
-		ModLoader.AddName(lightningStaff, "Lightning Staff");
-		ModLoader.AddName(explosiveStaff, "Explosive Staff");
-		ModLoader.AddName(fireStaff, "Fire Staff");
-		ModLoader.AddName(holyStaff, "Holy Staff");
-		ModLoader.AddName(teleportStaff, "Teleportation Staff");
-		ModLoader.AddName(wizardHat, "Wizard's Hat");
-		
+		/* Setting up item icons */
+		// TODO: move the icon override into the ItemClassArmor and ItemClassWeapon constructors
 		lightningStaff.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/wizardStaff.png");
 		explosiveStaff.iconIndex = lightningStaff.iconIndex;
 		fireStaff.iconIndex = lightningStaff.iconIndex;
 		holyStaff.iconIndex = lightningStaff.iconIndex;
 		teleportStaff.iconIndex = lightningStaff.iconIndex;
-		
-		
-		/* Setting up entities */
-		ModLoader.RegisterEntityID(EntityGlowingOrb.class, "glowingorb", mod_Classes.getUniqueEntityId());
+		wizardHat.iconIndex = ModLoader.addOverride("/gui/items.png", "/gui/wizardHat.png");
 		
 		
 		/* Setting up recipes */
@@ -89,6 +67,10 @@ public class mod_Wizard extends BaseMod
 		ModLoader.AddRecipe(new ItemStack(teleportStaff, 1), new Object[] {
 			"X", "Y", "Y", Character.valueOf('X'), Item.field_35416_bo, Character.valueOf('Y'), Item.bone
 		});
+		
+		
+		/* Setting up entities */
+		ModLoader.RegisterEntityID(EntityGlowingOrb.class, "glowingorb", mod_Classes.getUniqueEntityId());
 	}
 	
 	@Override
